7f1a7c08b8a1c2baf0919dcb5ff736c6bfe34588
TheoWolf <nezzar.reda@gmail.com>
2021-12-30 20:31:27 -0500

    Return to nodejs 4.4.5 era

--- src/index.js
+++ src/index.js
@@ -11,18 +11,18 @@ const port = process.env.PORT || 3000
 const datacite_url = process.env.DATACITE_URL || 'https://api.datacite.org'
 
 
-const encodeBase64 = (str) => {
+const encodeBase64 = function (str) {
     return Buffer.from(str).toString('base64')
 }
 
-const AuthorizarionHeader = `Basic ${encodeBase64(`${process.env.DATACITE_USERNAME}:${process.env.DATACITE_PASSWORD}`)}`
+const AuthorizarionHeader = "Basic " + encodeBase64(process.env.DATACITE_USERNAME + ":" + process.env.DATACITE_PASSWORD)
 
-const parseTextBody = (str) => {
+const parseTextBody = function (str) {
     const resources = str.substring(str.indexOf('<resource'), str.indexOf('</resource>') + 11)
     return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + resources.replace(/\\"/g, '\"').replace(/\\n/g,'')
 }
 
-const buildPayload = (encodeXml, doi) => {
+const buildPayload = function (encodeXml, doi) {
     return { 
         data: {
             id: doi,
@@ -37,102 +37,94 @@ const buildPayload = (encodeXml, doi) => {
     }
 }
 
-app.get('/status', async (req, res) => {
-    try {
-        console.log('REQUEST GET: /status')
-        const response =  await fetch(
-            `${datacite_url}/heartbeat`,
-            {
-                method: 'GET',
-                headers: { 'Accept': 'text/plain' }
-            }
-        )
-
+app.get('/status', function (req, res) {
+    console.log('REQUEST GET: /status')
+    return fetch(
+        datacite_url + "/heartbeat",
+        {
+            method: 'GET',
+            headers: { 'Accept': 'text/plain' }
+        }
+    ).then((response) => {
         if (response.status === 200) {
             return res.status(200).send('"success" : "Datacite is up"')
         }
-    } catch (error) {
+    }).catch((error) => {
         console.error('GET: /status Error', error)
-    }
-
-    res.status(400).send('"error": "Datacite is down"')
+        return res.status(400).send('"error": "Datacite is down"')
+    })
 })
 
-app.post('/id/*', async (req, res) => {
+app.post('/id/*', function (req, res) {
     const doi = req.originalUrl.substring(4).replace(/^(doi:)/,'')
     const body = parseTextBody(req.body)
 
-    try {
-        console.log(`REQUEST POST: /id/doi:${doi}`)
-        const response = await fetch(
-            `${datacite_url}/dois`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/vnd.api+json',
-                    'Authorization': AuthorizarionHeader              
-                },
-                body: JSON.stringify(buildPayload(encodeBase64(body), doi))
-            }
-        )
-
+    console.log("REQUEST POST: /id/doi:" + doi)
+    return fetch(
+        datacite_url + "/dois",
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Authorization': AuthorizarionHeader              
+            },
+            body: JSON.stringify(buildPayload(encodeBase64(body), doi))
+        }
+    ).then((response) => {
         if (response.status === 201) {
-            const result = await response.json()
-            return res.status(201).send(`success : doi${doi} \n datacite : ${JSON.stringify(result)}`)
+            return response.json().then((data) => {
+                return res.status(201).send("success : doi:" + doi + " \n " + "datacite : " + JSON.stringify(data))
+            })   
         }
-        
-    } catch (error) {
-        console.error(`POST: /id/doi:${doi} Error`, error)
-    }
-
-    res.status(400).send('error: "DOI NOT POSTED"')
+    }).catch((error) => {
+        console.error("POST: /id/doi:" + doi + "Error", error)
+        return res.status(400).send('error: "DOI NOT POSTED"')
+    })    
 })
 
-app.put('/id/*', async (req, res) => {
+app.put('/id/*', function (req, res) {
     const doi = req.originalUrl.substring(4).replace(/^(doi:)/,'')
     const body = parseTextBody(req.body)
-
-    try {
-        console.log(`REQUEST PUT: /id/doi:${doi}`)
-        const response = await fetch(
-            `${datacite_url}/dois/${doi}`,
-            {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/vnd.api+json',
-                    'Authorization': AuthorizarionHeader              
-                },
-                body: JSON.stringify(buildPayload(encodeBase64(body), doi))
-            }
-        )
-
-        if (response.status === 200) {
-            const result = await response.json()
-            return res.status(200).send(`success : doi${doi} \n datacite: ${JSON.stringify(result)}`)
+    
+    console.log("REQUEST PUT: /id/doi:" + doi)
+    return fetch(
+        datacite_url + "/dois/" + doi,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Authorization': AuthorizarionHeader              
+            },
+            body: JSON.stringify(buildPayload(encodeBase64(body), doi))
         }
-    } catch (error) {
-        console.error(`PUT: /id/doi:${doi} Error`, error)
-    }
-
-    res.status(400).send('error: "DOI NOT UPDATED"')
+    ).then((response) => {
+        if (response.status === 200) {
+            return response.json().then((data) => {
+                return res.status(200).send("success : doi:" + doi + " \n " + "datacite : " + JSON.stringify(data))
+            })
+        }
+    }).catch((error) => {
+        console.error("PUT: /id/doi:" + doi + "Error", error)
+        return res.status(400).send('error: "DOI NOT UPDATED"')
+    })  
 })
 
 // Catch all other requests and log them
 app.get('/*', (req, res) => {
-    console.log(`GET: ${req.originalUrl} - Not implemented`)
+    console.log("GET: " + req.originalUrl + " - Not implemented")
     res.status(400).send('error: "ENDPOINT NOT IMPLEMENTED"')
 })
 
 app.put('/*', (req, res) => {
-    console.log(`PUT: ${req.originalUrl} - Not implemented`)
+    console.log("PUT: " + req.originalUrl + " - Not implemented")
     res.status(400).send('error: "ENDPOINT NOT IMPLEMENTED"')
 })
 
 app.post('/*', (req, res) => {
-    console.log(`POST: ${req.originalUrl} - Not implemented`)
+    console.log("POST: " + req.originalUrl + " - Not implemented")
     res.status(400).send('error: "ENDPOINT NOT IMPLEMENTED"')
 })
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
+    console.log("Listening at http://localhost:" + port)
 })
